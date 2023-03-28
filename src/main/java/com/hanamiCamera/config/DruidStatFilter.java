ff5b4b0720619d9d9cd8244a6eb542591b6dcef3
YOKI1216 <1151509140@qq.com>
2023-03-28 14:53:28 +0800

    配置druid并且降版本到2.7.10

---
+++ src/main/java/com/hanamiCamera/config/DruidStatFilter.java
@@ -0,0 +1,11 @@
+package com.hanamiCamera.config;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+/**
+ * @Description :
+ * @Author : YokiWare
+ * @Date: 2023/3/28  14:43
+ */
+public class DruidStatFilter extends WebStatFilter {
+}
